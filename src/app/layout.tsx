6bffb9c499bbdf48bb829d623873e10c0b7e875d
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-30 18:18:47 -0500

    :shipit:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,23 @@ const redirectToUserProfile = (username: string) => {
 };
 
 export default function Home() {
-  const [user, setUser] = useState < string > ("");
+  const [user, setUser] = useState<string>("");
   return (
-    <main className="flex flex-col min-h-screen text-white bg-black p-24">
-      <h1>gitpilled</h1>
-      Search for a user to see their top languages
-      <br />
+    <main className="flex flex-col justify-center items-center min-h-screen gap-8 text-white bg-black p-24">
+      <h2 className="text-4xl">What language are you pilled in?</h2>
       <form action={() => redirectToUserProfile(user)}>
         <input
           name="user"
           value={user}
           onChange={(e) => setUser(e.target.value)}
           data-disable-1password="true"
-          className="bg-black"
-          placeholder="Enter a github username"
+          className="border border-neutral-600 bg-black p-2 rounded-lg text-4xl"
+          placeholder="Github username"
           type="text"
         />
+        <button className="bg-blue-700 rounded-r-xl p-2 text-4xl">Search</button>
       </form>
+      
     </main>
   );
 }
