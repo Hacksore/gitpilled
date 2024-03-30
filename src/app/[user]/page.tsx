eb532bd9e359c8ad2c014cae0f4b403591b90f38
typesafeui <typesafeui@gmail.com>
2024-03-31 00:24:11 +0100

    feat: better+++

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -1,9 +1,14 @@
 import { Octokit } from "@octokit/rest";
+import colors from "@/utils/colors.json";
+import { useMemo } from "react";
+
+type LanguageName = keyof typeof colors;
+const DEFAULT_COLOR = "#444444";
 
 async function getUsersTopLanguages(username: string) {
   const octokit = new Octokit({
     // TODO: add the way to round robin the tokens here
-    // auth: process.env.GITHUB_AUTH_TOKEN,
+    auth: process.env.GITHUB_AUTH_TOKEN,
   });
 
   const listOfRepos = await octokit.paginate("GET /users/{username}/repos", {
@@ -27,7 +32,7 @@ async function getUsersTopLanguages(username: string) {
       }
       return acc;
     },
-    {},
+    {}
   );
 
   // sort the languages by count and have name and count as a property
@@ -45,16 +50,38 @@ export default async function Home({
 }) {
   const languages = await getUsersTopLanguages(user);
 
+  const maxCount = languages[0].count;
+  const languagesWithPercentage = languages.map((lang) => ({
+    ...lang,
+    percentage: 25 + Math.floor((lang.count / maxCount) * 100) * 0.75,
+  }));
+
   return (
-    <main className="min-h-screen text-white bg-black p-24">
-      <h2 className="text-4xl font-bold">Top languages for {user}</h2>
-      <ul>
-        {languages.map((lang) => (
-          <li key={lang.name}>
-            {lang.name}: {lang.count}
-          </li>
+    <main className="w-full h-full flex flex-col bg-gradient-to-b overflow-auto from-[#131313] to-black text-white">
+      <h2 className="text-3xl mt-16 text-center">
+        <span className="font-bold text-4xl">@{user}</span> is pilled on
+      </h2>
+      <div className="flex flex-grow pt-24 px-32 gap-24 w-fit mx-auto">
+        {languagesWithPercentage.map((lang) => (
+          <div
+            key={lang.name}
+            className="h-full flex flex-col items-center justify-end gap-12"
+          >
+            <div className="text-center text-3xl font-extrabold">
+              {lang.name}
+            </div>
+            <div
+              className="rounded-t-3xl h-full w-12 md:w-48"
+              style={{
+                backgroundColor:
+                  colors[lang.name.toLocaleLowerCase() as LanguageName].color ||
+                  DEFAULT_COLOR,
+                height: `${lang.percentage}%`,
+              }}
+            ></div>
+          </div>
         ))}
-      </ul>
+      </div>
     </main>
   );
 }
