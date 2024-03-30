24e056d3e56963deec8cd30193626700ad54a30f
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-30 18:04:25 -0500

    Add </Analytics>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
