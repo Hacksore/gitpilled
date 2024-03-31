81bb62e390377b5d1cfea52c036fb8f623b8bf48
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-31 14:05:50 -0500

    Render usename

--- src/app/[user]/error.tsx
+++ src/app/[user]/error.tsx
@@ -2,16 +2,19 @@
 
 import { useEffect } from "react";
 import UserStats from "../component/user-stats";
+import { useParams } from "next/navigation";
 
 export default function Error({
   error,
 }: {
   error: Error & { digest?: string };
 }) {
+  const params = useParams<{ user: string  }>();
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
-  return <UserStats user="error" githubData={undefined} />;
+  return <UserStats user={params.user} githubData={undefined} />;
 }
