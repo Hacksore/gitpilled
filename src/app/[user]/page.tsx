5d9a20661983c3080d0b33026f540389ed4d1dfd
typesafeui <typesafeui@gmail.com>
2024-03-31 03:31:58 +0200

    feat: packages

--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -1,7 +1,16 @@
 import { Suspense } from "react";
 import UserStats from "../component/user-stats";
 import FetchRepos from "../fetch-repos";
+import { Metadata } from "next";
+
 /* eslint-disable @next/next/no-img-element */
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://gitpilled.vercel.app"
+  ),
+};
 
 export default function Home({
   params: { user },
