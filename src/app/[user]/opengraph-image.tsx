77d43864e78d4950dc7e42be2407f313fe07a01d
typesafeui <typesafeui@gmail.com>
2024-03-31 01:55:42 +0100

    feat: sts

--- src/app/[user]/opengraph-image.tsx
+++ src/app/[user]/opengraph-image.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 import colors from "@/utils/colors.json";
-import { DEFAULT_COLOR, LanguageName } from "@/utils";
+import { DEFAULT_COLOR, LanguageName, getUsersTopLanguages } from "@/utils";
 // Route segment config
 export const runtime = "edge";
 
@@ -24,49 +24,155 @@ export default async function Image(props: {
   //     new URL("./Inter-SemiBold.ttf", import.meta.url)
   //   ).then((res) => res.arrayBuffer());
   const user = props.params.user;
-  const languagesWithPercentage = [] as any[];
 
   const bast64UserImage = await fetch(`https://github.com/${user}.png`)
     .then((r) => r.arrayBuffer())
     .then((r) => Buffer.from(r).toString("base64"));
 
+  // const languages = await getUsersTopLanguages(user);
+
+  const languages = [
+    {
+      name: "JavaScript",
+      count: 10,
+    },
+    {
+      name: "TypeScript",
+      count: 8,
+    },
+    {
+      name: "Python",
+      count: 5,
+    },
+    {
+      name: "Java",
+      count: 3,
+    },
+    {
+      name: "C++",
+      count: 2,
+    },
+  ];
+
+  const maxCount = languages[0].count;
+  const languagesWithPercentage = languages.map((lang) => ({
+    ...lang,
+    percentage: 25 + Math.floor((lang.count / maxCount) * 100) * 0.75,
+  }));
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
         style={{
-          backgroundColor: "#131313",
           width: "100%",
           height: "100%",
           display: "flex",
           flexDirection: "column",
-          gap: 12,
           alignItems: "center",
           justifyContent: "center",
+          /* grid using gradient */
+          backgroundImage: `linear-gradient(to bottom, #131313, #0a0a0a)`,
         }}
       >
-        <img
-          alt="Logo"
-          height={120}
-          width={120}
-          style={{
-            marginTop: 24,
-            borderRadius: "50%",
-            objectFit: "cover",
-          }}
-          src={`data:image/png;base64,${bast64UserImage}`}
-        />
         <div
           style={{
             width: "100%",
+            height: "100%",
             display: "flex",
-            flexGrow: 1,
-            gap: 12,
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            padding: 12,
+            /* grid using gradient */
+            backgroundSize: "100px 100px",
+            backgroundImage: `linear-gradient(to right, #ffffff10 1px, transparent 1px),
+          linear-gradient(to bottom, #ffffff10 1px, transparent 1px)`,
           }}
-        ></div>
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 16,
+              color: "white",
+              fontSize: 48,
+            }}
+          >
+            <img
+              alt="Logo"
+              height={120}
+              width={120}
+              style={{
+                marginTop: 24,
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+              src={`data:image/png;base64,${bast64UserImage}`}
+            />
+            @{user}
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "50%",
+              display: "flex",
+              flexGrow: 1,
+              gap: 12,
+              alignItems: "center",
+              justifyContent: "center",
+              padding: 0,
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "flex-end",
+                justifyContent: "center",
+                gap: 64,
+                height: "100%",
+              }}
+            >
+              {languagesWithPercentage.map((lang, i) => (
+                <div
+                  key={lang.name}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    height: "100%",
+                    justifyContent: "flex-end",
+                    gap: 32,
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      fontSize: 32,
+                      color: "white",
+                    }}
+                  >
+                    {lang.name}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      height: `${lang.percentage * 0.85}%`,
+                      width: 120,
+                      backgroundColor:
+                        colors[lang.name.toLocaleLowerCase() as LanguageName]
+                          .color || DEFAULT_COLOR,
+                      borderRadius: "32px 32px 0 0",
+                    }}
+                  ></div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     ),
     // ImageResponse options
--- src/app/[user]/page.tsx
+++ src/app/[user]/page.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import UserStats from "../component/user-stats";
 import FetchRepos from "../fetch-repos";
+/* eslint-disable @next/next/no-img-element */
 
 export default function Home({
   params: { user },
 }: {
   params: { user: string };
 }) {
-
   return (
     <main className="w-full h-full flex flex-col items-center bg-gradient-to-b overflow-auto from-[#131313] to-black text-white">
       <Suspense fallback={<UserStats loading />}>
