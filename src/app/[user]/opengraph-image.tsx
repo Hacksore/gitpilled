da358b8a236fb4d70706a16e4500b6b394c6251c
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-31 14:37:03 -0500

    404 should still og

--- src/app/[user]/opengraph-image.tsx
+++ src/app/[user]/opengraph-image.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/og";
 import colors from "@/utils/colors.json";
@@ -25,20 +26,54 @@ export default async function Image(props: {
   };
 }) {
   const { user } = props.params;
+
+  let res;
+  try {
+    res = await getUsersTopLanguages(user);
+  } catch (e) {
+    // do nothing and eat the error 
+  }
+
+  if (!res) {
+    const rawImageData = await fetch(
+      new URL("../opengraph-image.png", import.meta.url),
+    ).then((res) => res.arrayBuffer());
+
+    const imageData = `data:image/png;base64,${Buffer.from(
+      rawImageData,
+    ).toString("base64")}`;
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            display: "flex",
+            background: "#f6f6f6",
+            width: "100%",
+            height: "100%",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={imageData} />
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  }
+
+  const { pilledLanguages, username } = res;
   const base64UserImage = await fetch(`https://github.com/${user}.png`)
     .then(async (r) => r.blob())
     .then(
       async (blob) =>
         `data:${blob.type};base64,${Buffer.from(
-          await blob.arrayBuffer()
-        ).toString("base64")}`
+          await blob.arrayBuffer(),
+        ).toString("base64")}`,
     );
-
-  const res = await getUsersTopLanguages(user);
-  if (!res) {
-    return notFound();
-  }
-  const { pilledLanguages, username } = res;
 
   return new ImageResponse(
     (
@@ -204,6 +239,6 @@ export default async function Image(props: {
     // ImageResponse options
     {
       ...size,
-    }
+    },
   );
 }
