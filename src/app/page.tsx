c90c207a6efcdba06959a0f43694b055161c70ed
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-03-30 19:05:54 -0500

    Add pill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   const [user, setUser] = useState<string>("");
   return (
     <main className="flex flex-col justify-center items-center min-h-screen gap-8 text-white bg-black p-24">
+      <img src="/pill.png" alt="pill" className="w-20 h-20" />
       <h2 className="text-4xl">What language are you pilled in?</h2>
       <form action={() => redirectToUserProfile(user)}>
         <input
