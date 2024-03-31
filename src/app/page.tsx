4a9a8da978495d3aec32b59fd336fbc0737b2d61
typesafeui <typesafeui@gmail.com>
2024-03-31 15:40:50 +0200

    fix: logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { redirect } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { GitPilledLogo } from "@/components/logo";
 
 const redirectToUserProfile = (username: string) => {
   return redirect("/" + username);
@@ -23,11 +24,14 @@ export default function Home() {
       >
         <div className="flex flex-col items-center bg-background p-8 mx-2 md:p-12 gap-2 rounded-xl border border-primary/10">
           <img src="/pill.png" alt="pill" className="w-24 h-24" />
-          <h1 className="text-3xl font-bold">gitpilled</h1>
+          <GitPilledLogo className="h-8 w-fit mt-2 " />
           <h2 className="text-xl font-bold">
             What language are you pilled in?
           </h2>
-          <form action={() => redirectToUserProfile(user)} className="flex mt-8">
+          <form
+            action={() => redirectToUserProfile(user)}
+            className="flex mt-8"
+          >
             <Input
               name="user"
               value={user}
